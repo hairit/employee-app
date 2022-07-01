b33a2c2a0a76758ecff10852cabb99513b3f6369
Tuong Hai <tuonghai.contact@gmail.com>
2022-07-01 11:43:18 +0700

    update searchEmployeesByName function

--- src/Data/API.js
+++ src/Data/API.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 const call = (method, path, body) => {
   if (
-    (method === "PUT" || method === "POST" || method === "PATCH") &&
+    (method === 'PUT' || method === 'POST' || method === 'PATCH') &&
     body === null
   ) {
-    console.log("This method need body data");
+    console.log('This method need body data');
     return;
   }
   return axios({
     method: method,
-    url: process.env.REACT_APP_URL_LOCAL + "/" + path,
+    url: process.env.REACT_APP_URL + '/' + path,
     data: body,
   });
 };
--- src/Pages/Employee.js
+++ src/Pages/Employee.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import '../CSS/Employee.css';
 import call from '../Data/API';
@@ -10,7 +10,6 @@ import { AiOutlineLeft } from 'react-icons/ai';
 import { RiEdit2Fill } from 'react-icons/ri';
 import { ImSearch } from 'react-icons/im';
 import { Button } from 'react-bootstrap';
-import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import AddEmployee from '../Form/AddEmployee';
 import {
@@ -25,40 +24,26 @@ const GET_METHOD = 'GET';
 const POST_METHOD = 'POST';
 const PUT_METHOD = 'PUT';
 const DELETE_METHOD = 'DELETE';
-const PATCH_METHOD = 'PATCH';
+// const PATCH_METHOD = 'PATCH';
 const EMPLOYEE_API = 'api/employee';
 
 export default function Employee() {
   const [loading, setLoading] = useState(false);
   const [totalRows, setTotalRows] = useState(0);
+  const [pagination, setPagination] = useState(true);
   const [perPage, setPerPage] = useState(10);
-  const [page, setPage] = useState(1);
   const [openFormAddEmployee, setOpenFormAddEmployee] = useState(false);
   const [searchEmployeesByName, setSearchEmployeesByName] = useState('');
   const [employee, setEmployee] = useState(null);
   const [employeesData, setEmployeesData] = useState([]);
-  const [reload, setReload] = useState(true);
-
   const employees = useSelector((state) => state.employee.employees);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   fetchEmployees(perPage, page);
-  // }, []);
-
-  // const fetchEmployees = () => {
-  //   call(GET_METHOD, EMPLOYEE_API, null)
-  //     .then((res) => {
-  //       dispatch(setEmployees(res.data));
-  //       employees && setEmployeesData(employees);
-  //     })
-  //     .catch((err) => console.log('Cannot get employees' + err));
-  // };
   const fetchEmployees = (perPage, page) => {
     setLoading(true);
     var paging = '';
     page && perPage
-      ? (paging = `/page/${page}/amount/${perPage}`)
+      ? (paging = `/page/${page}/perPage/${perPage}`)
       : (paging = '');
     call(GET_METHOD, EMPLOYEE_API + paging, null)
       .then((res) => {
@@ -70,48 +55,16 @@ export default function Employee() {
       .catch((err) => console.log(err));
   };
 
-  const deleteHandleClick = (_id) => {
-    const options = {
-      title: 'Notification',
-      message: 'Do you want to delete this employee',
-      buttons: [
-        {
-          label: 'Yes',
-          onClick: () => removeEmployee(_id),
-        },
-        {
-          label: 'No',
-          onClick: () => {},
-        },
-      ],
-      closeOnEscape: true,
-      closeOnClickOutside: true,
-      keyCodeForClose: [8, 32],
-      willUnmount: () => {},
-      afterClose: () => {},
-      onClickOutside: () => {},
-      onKeypress: () => {},
-      onKeypressEscape: () => {},
-      overlayClassName: 'overlay-custom-class-name',
-    };
-    confirmAlert(options);
-  };
-
   const removeEmployee = (_id) => {
     call(DELETE_METHOD, EMPLOYEE_API + `/${_id}`, null)
       .then((res) => {
         if (res.status === 200) {
-          //console.log(res.data);
-          console.log(res.data);
           dispatch(deleteEmployee(res.data));
-
           const employeeFilter = employees.filter((element) => {
             return element._id !== res.data._id;
           });
-
           dispatch(setEmployees(employeeFilter));
           setEmployeesData(employeeFilter);
-          //fetchEmployees(perPage, page)
         }
       })
       .catch((err) => alert('Delete employee fail' + err.message));
@@ -152,14 +105,18 @@ export default function Employee() {
     //   setOpenFormAddEmployee(false);
     // }
   };
+
   const searchEmployees = () => {
-    if (!searchEmployeesByName) {
-      fetchEmployees();
-    } else {
-      call(GET_METHOD, EMPLOYEE_API + `/name/${searchEmployeesByName}`, null)
-        .then((res) => dispatch(setEmployees(res.data)))
-        .catch((err) => console.log('Can not search employees' + err));
-    }
+    setLoading(true);
+    call(GET_METHOD, EMPLOYEE_API + `/name/${searchEmployeesByName}`, null)
+      .then((res) => {
+        if (!searchEmployeesByName) setPagination(true);
+        else setPagination(false);
+        dispatch(setEmployees(res.data));
+        setEmployeesData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log('Can not search employees' + err));
   };
 
   const columns = [
@@ -223,7 +180,6 @@ export default function Employee() {
     },
   ];
   const handlePageChange = (page) => {
-    setPage(page);
     fetchEmployees(perPage, page);
   };
 
@@ -234,16 +190,12 @@ export default function Employee() {
 
   return (
     <div className="employee">
-      {/* <div>{employees?.length}</div> */}
-      {/* {console.log(employeesData)}
-      // <div>{employeesData && employeesData[9]?.sex}</div> */}
-      {console.log({ employeesData: employeesData })}
       <DataTable
         style={{ zIndex: 10 }}
         columns={columns}
         data={employeesData}
         fixedHeader
-        fixedHeaderScrollHeight="800px"
+        fixedHeaderScrollHeight="500px"
         highlightOnHover
         subHeader
         subHeaderComponent={
@@ -263,7 +215,7 @@ export default function Employee() {
         }
         subHeaderAlign="right"
         progressPending={loading}
-        pagination
+        pagination={pagination}
         paginationServer
         paginationTotalRows={totalRows}
         onChangeRowsPerPage={handlePerRowsChange}
@@ -284,7 +236,7 @@ export default function Employee() {
             }}
             onClick={() => {
               setOpenFormAddEmployee(!openFormAddEmployee);
-              //setEmployee(null);
+              setEmployee(null);
             }}
           ></AiOutlineLeft>
         </div>
